fcebe82d1b193e6cecd8388e9a5963053ed0538a
AlekseyVY <alekseyvy@gmail.com>
2021-07-16 14:02:15 +0300

    feature: task completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.answer = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.answer = Math.ceil((this.max  + this.min) / 2)
+        return this.answer
     }
 
     lower() {
-
+        this.max = this.answer;
     }
-
+    
     greater() {
-
+        this.min = this.answer;
     }
 }
 
